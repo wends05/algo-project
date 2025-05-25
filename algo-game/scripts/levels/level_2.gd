034528b056b66047a009e6cc07675e73ce20289d
wends05 <wendellterence.dador-23@cpu.edu.ph>
2025-05-25 08:26:50 +0800

    level 2 to level 3 script, added extra nodes on level 3

--- algo-game/scripts/levels/level_2.gd
+++ algo-game/scripts/levels/level_2.gd
@@ -4,4 +4,4 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.get_parent() is Player:
 		print("IS PLAYER")
 		
-		get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+		get_tree().change_scene_to_file("res://scenes/levels/level_3.tscn")
