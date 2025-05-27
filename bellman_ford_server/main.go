6428490e26d04233541851d9823fc9b7b3da9352
wends05 <wendellterence.dador-23@cpu.edu.ph>
2025-05-28 05:15:07 +0800

    port from env if available

--- bellman_ford_server/main.go
+++ bellman_ford_server/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	// gin
 	c "bellman_ford_server/controllers"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	router := gin.Default()
 
 	router.POST("/bellmanford", c.BellmanFord)
 
 	router.GET("/graph", c.GenerateRandomGraph)
 
-	router.Run(":8080")
+	router.Run(":" + port)
 }
